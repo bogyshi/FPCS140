b55ff957aa7f7c419f9a9c8314224abcba192716
bogyshi <avanroi1@binghamton.edu>
2015-04-28 16:00:51 -0400

    weather api and json stuff

---
+++ weather.hs
@@ -0,0 +1,109 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module WeatherStuff where
+
+import Control.Monad
+import Data.Aeson
+import Data.Functor
+import Data.Maybe
+import Data.String
+import Network.Curl
+import Data.Text.Internal
+import Data.Functor.Identity
+import Data.Monoid
+import Control.Applicative
+import Data.String
+
+
+data Weather = Weather
+             { dt :: Integer
+             , coord :: Coord
+             , ws :: [W]
+             , wmain :: Main
+	     , c :: Clouds
+             } deriving (Show)
+
+data Clouds = Clouds
+    { coverage :: Int
+    } 
+
+data Coord = Coord
+           { lon :: Int
+           , lat :: Int
+           } deriving (Show)
+
+data W = W
+       { w_id :: Int
+       , w_main :: String
+       , w_description :: String
+       } deriving (Show)
+
+data Main = Main
+    { temp :: Int
+    } deriving (Show)
+
+
+instance FromJSON Clouds where
+    parseJSON (Object v) = Clouds
+                           <$> v .: "all"
+
+instance FromJSON Weather where
+    parseJSON (Object v) = Weather
+                           <$> v .: "dt"
+                           <*> v .: "coord"
+                           <*> v .: "weather"
+                           <*> v .: "main"
+			   <*> v .: "clouds"
+
+instance FromJSON Coord where
+    parseJSON (Object v) = Coord
+                           <$> v .: "lon"
+                           <*> v .: "lat"
+
+instance FromJSON W where
+    parseJSON (Object v) = W
+                           <$> v .: "id"
+                           <*> v .: "main"
+                           <*> v .: "description"
+
+instance ToJSON W where
+    toJSON (W i m d) = object ["id" .= i, "main" .= m, "description" .= d]
+
+--to JSON is only if you need to according to the weather api, so not really necessary.
+
+instance FromJSON Main where
+    parseJSON (Object v) = Main
+                           <$> v .: "temp"
+
+instance Show Clouds where 
+	 show (Clouds c) = (show (c::Int))
+
+getCover :: Clouds -> Int
+getCover (Clouds a) = a
+
+compute :: Int -> String
+compute 0 = "notta problemo"
+compute a | a >= 80  = "tis shady"
+	  | otherwise = "nvm"
+--get rushils get and post request webstuff
+
+main = do
+  x <- getLine
+  response <- snd <$> curlGetString ("http://api.openweathermap.org/data/2.5/weather?zip="++x++",us") []
+  let w = decode (fromString response)
+  let coverage =  (c (fromJust (w :: Maybe Weather)))
+  print coverage
+  --mapM_ print . T.map w_description . ws . fromJust $ (w :: Maybe Weather)
+  print $ compute (getCover coverage)
+ -- mapM_ print . ws . fromJust $ ((fromString response) :: Maybe Weather)
+
+
+--decod is how you take a json to a data type 
+--decode:: (fromJSON a) => String -> a
+-- if you have fromJSON erson where
+-- decode of bytestring into a datatype. 
+--just go to aason to figure this stuff out.
+
+-- there is ap roof language like idris, and its wrtiten in haskell, its agda, and its works similar, jsut for theorems and proofs. The most popular one is made by the the french, its call coq.
+
+--etherpad for technical interviews
